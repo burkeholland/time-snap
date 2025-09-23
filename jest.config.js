f0beada27eb168dece63797ca3f72ea89e68ae78
Burke Holland <burkeholland@gmail.com>
2025-09-23 18:24:03 -0500

    remove node_modules 🤦

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  testEnvironment: 'node',
-  roots: ['<rootDir>/tests'],
-  moduleFileExtensions: ['js', 'json'],
-  collectCoverageFrom: ['src/**/*.js'],
-  clearMocks: true
-};
